dc913a581e7c2041dee818373afaf1cbd5f0f097
Madni <thezubairkhan.developer@gmail.com>
2025-08-11 18:16:31 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -20,95 +20,90 @@ import NotFound from "./generic-components/PageNotFound";
 import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
-  const accessToken = localStorage.getItem("access_token");
-  console.log("accessToken", accessToken);
-
   return (
     <AppInitializer>
       <Router>
         <ScrollToTop />
         <ToastProvider>
-          <AuthProvider>
-            <div className="App">
-              <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
-              <Routes>
-                {/* Public routes */}
-                <Route path="/auth/signup" element={<SignUpPage />} />
-                <Route path="/auth/login" element={<SignInPage />} />
-                <Route
-                  path="/auth/confirm-email"
-                  element={<ConfirmEmailPage />}
-                />
-                <Route
-                  path="/auth/forgot-password"
-                  element={<ForgotPasswordPage />}
-                />
-                <Route
-                  path="/auth/reset-password"
-                  element={<ResetPasswordPage />}
-                />
-                <Route path="*" element={<NotFound />} />
+          {/* <AuthProvider> */}
+          <div className="App">
+            <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+            <Routes>
+              {/* Public routes */}
+              <Route path="/auth/signup" element={<SignUpPage />} />
+              <Route path="/auth/login" element={<SignInPage />} />
+              <Route
+                path="/auth/confirm-email"
+                element={<ConfirmEmailPage />}
+              />
+              <Route
+                path="/auth/forgot-password"
+                element={<ForgotPasswordPage />}
+              />
+              <Route
+                path="/auth/reset-password"
+                element={<ResetPasswordPage />}
+              />
 
-                {/* Protected routes */}
-                <Route
-                  path="/personal-account-free"
-                  element={
-                    <ProtectedRoute>
-                      <BeforeSubsciption />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/after-subscription"
-                  element={
-                    <ProtectedRoute>
-                      <AfterSubsciption />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/plans"
-                  element={
-                    <ProtectedRoute>
-                      <SubscriptionPlans />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/take-test"
-                  element={
-                    <ProtectedRoute>
-                      <TakeTestPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/success"
-                  element={
-                    <ProtectedRoute>
-                      <Success />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/cancel"
-                  element={
-                    <ProtectedRoute>
-                      <Cancel />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/results"
-                  element={
-                    <ProtectedRoute>
-                      <ResultsPage />
-                    </ProtectedRoute>
-                  }
-                />
-              </Routes>
-            </div>
-          </AuthProvider>
+              {/* Protected routes */}
+              <Route
+                path="/personal-account-free"
+                element={<BeforeSubsciption />}
+              />
+              <Route
+                path="/after-subscription"
+                element={
+                  <ProtectedRoute>
+                    <AfterSubsciption />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/plans"
+                element={
+                  <ProtectedRoute>
+                    <SubscriptionPlans />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/take-test"
+                element={
+                  <ProtectedRoute>
+                    <TakeTestPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/success"
+                element={
+                  <ProtectedRoute>
+                    <Success />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/cancel"
+                element={
+                  <ProtectedRoute>
+                    <Cancel />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/results"
+                element={
+                  <ProtectedRoute>
+                    <ResultsPage />
+                  </ProtectedRoute>
+                }
+              />
+
+              {/* Catch all unmatched routes */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+          {/* </AuthProvider> */}
         </ToastProvider>
       </Router>
     </AppInitializer>
